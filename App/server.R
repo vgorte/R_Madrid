22f6ba1f07a94da01c796d536f9fa502576d2203
viktorgorte <v_gort01@uni-muenster.de>
2017-12-05 16:17:22 +0100

    restructuring

--- App/server.R
+++ App/server.R
@@ -1,7 +1,44 @@
 library(shiny)
+library(rgdal)
 
-# Define server logic 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  ##SHAPEFILES
+
+  direction <- eventReactive(input$direction, {
+    directions <- readOGR("./directions/", "directions")
+    proj4string(directions) <- CRS("+proj=utm +zone=30 +ellps=GRS80 +units=m +no_defs")
+    directions_longlat <- spTransform(directions, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+    return(directions_longlat)
+  })
   
+  madridOutline<- eventReactive(input$madrid, {
+    madridShape <- readOGR("./madridshapefile/", "Distritos")
+    madridShape <- spTransform(madridShape, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+    return(madridShape)
+  })
     
+  visibilityMadridOutlines<- eventReactive(input$madrid, {
+    if(input$madrid == TRUE){
+      print("on")
+      return(0.1)
+    }else{
+      print("off")
+      return(0)
+    }
+  })
+    
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron,
+                       options = providerTileOptions(noWrap = )
+      ) %>%
+      addPolygons(data=direction(),weight=1,col = 'red') %>% 
+      addPolygons(data=madridOutline(),weight=visibilityMadridOutlines(),col = 'black') %>% 
+      
+      setView(lng = -3.8196207,
+              lat = 40.4678698,
+              zoom = 10)
+  
+  })
 })
+
--- App/ui.R
+++ App/ui.R
@@ -1,57 +1,14 @@
-setwd("/Users/user/Documents/R_Madrid/App/")
-library(sp)
 library(shiny)
 library(leaflet)
-library(rgdal)
-
-createMap <- function(){
-  ##SHAPEFILES
-  madridShape <- readOGR("./madridshapefile/", "Distritos")
-  directions <- readOGR("./directions/", "directions")
-  
-  
-
-  ##TRANSFORMATIONS
-  #Madrid (Outlines)
-  madridShape <- spTransform(madridShape, CRS("+proj=longlat +datum=WGS84 +no_defs"))
-  #Traffic directions
-  proj4string(directions) <- CRS("+proj=utm +zone=30 +ellps=GRS80 +units=m +no_defs")
-  directions_longlat <- spTransform(directions, CRS("+proj=longlat +datum=WGS84 +no_defs"))
-  
-  map <- leaflet() %>% 
-    addProviderTiles("CartoDB.Positron") %>% 
-
-    addPolygons(data=madridShape,weight=1,col = 'black') %>% 
-    addPolygons(data = directions_longlat, weight=1, col = 'red') %>% 
-    
-    
-    setView(lng = -3.8196207,
-            lat = 40.4678698,
-            zoom = 10)
-  return(map)
-}
-
-
-# Define UI for application that plots random distributions 
 shinyUI(
   fluidPage(
-    # Navbar
-    navbarPage("Madrid - Air quality & traffic ",
-    tabPanel("Map", 
-      fluidRow(
-        createMap(),
-        
-        checkboxInput(inputId = "traffic",
-                      label = strong("Traffic"),
-                      value = FALSE),
-        checkboxInput(inputId = "air",
-                      label = strong("Air Quality"),
-                      value = FALSE)
-      )
-    ),
-    tabPanel("Correlation"),
-    tabPanel("blub")
-    )
+    leafletOutput("mymap"),
+    checkboxInput(inputId = "direction",
+                  label = strong("Traffic Directions"),
+                  value = FALSE),
+    checkboxInput(inputId = "madrid",
+                  label = strong("Madrid Outlines"),
+                  value = FALSE)
   )
 )
 
