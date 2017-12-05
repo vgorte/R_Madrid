ff05ab5113006e735b7293cd86bae89b742956c4
viktorgorte <v_gort01@uni-muenster.de>
2017-12-05 16:51:58 +0100

    UI tabpanel & navbar

--- App/ui.R
+++ App/ui.R
@@ -2,13 +2,20 @@ library(shiny)
 library(leaflet)
 shinyUI(
   fluidPage(
-    leafletOutput("mymap"),
-    checkboxInput(inputId = "direction",
-                  label = strong("Traffic Directions"),
-                  value = FALSE),
-    checkboxInput(inputId = "madrid",
-                  label = strong("Madrid Outlines"),
-                  value = FALSE)
+    navbarPage("Madrid - Air quality & traffic ",
+      tabPanel("Map", 
+        fluidRow(
+          leafletOutput("mymap"),
+          checkboxInput(inputId = "direction",
+                        label = strong("Traffic Directions"),
+                        value = FALSE),
+          checkboxInput(inputId = "madrid",
+                        label = strong("Madrid Outlines"),
+                        value = FALSE)
+        )
+      ),
+      tabPanel("Other")
+    )
   )
 )
 
